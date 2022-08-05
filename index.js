f14696828df7ffe5932e1ba7708370b72ce667cc
anas <ktmanas2020@gmail.com>
2022-08-05 20:12:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const billInput = document.getElementById('billTotalInput');
+const tipInput = document.getElementById('numberOfPeople');
+const numberOfPeopleDiv = document.getElementById('numberOfPeople');
+const perPersonTotal = document.getElementById('perPersonTotal');
+
+console.log(billInput);
