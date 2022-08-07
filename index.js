f7c91b0f63522dbd249b301b56638a75d133fe1f
anas <ktmanas2020@gmail.com>
2022-08-07 09:46:38 +0530

    total bill

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
-const billInput = document.getElementById('billTotalInput');
-const tipInput = document.getElementById('numberOfPeople');
-const numberOfPeopleDiv = document.getElementById('numberOfPeople');
-const perPersonTotalDiv = document.getElementById('perPersonTotal');
-
+const billInput = document.getElementById("billTotalInput");
+const tipInput = document.getElementById("tipInput");
+const numberOfPeopleDiv = document.getElementById("numberOfPeople");
+const perPersonTotalDiv = document.getElementById("perPersonTotal");
 
 let numberOfPeople = Number(numberOfPeopleDiv.innerText);
 
 
 const calculateBill = () => {
-    const bill = Number(billInput.value);
-   
+  const bill = Number(billInput.value);
 
-    const  tip = Number(tipInput.value) / 100
- 
-    const tipAmount = bill * tip;
+  const tipPercentage = Number(tipInput.value) / 100;
 
-    console.log(tip)
+  const tipAmount = bill * tipPercentage;
 
-    
-}
- 
+  const total = tipAmount + bill;
 
-  
+  const perPersonTotal = total / numberOfPeople;
+
+  perPersonTotalDiv.innerText = `$${perPersonTotal.toFixed(2)}`;
+};
+
+const decreasePeople = () => {
+  if (numberOfPeople <= 1) {
+    return;
+  }
+  numberOfPeople -= 1;
+
+  numberOfPeopleDiv.innerText = numberOfPeople;
+};
+
+const increasePeople = () => {
+  numberOfPeople += 1;
+
+  numberOfPeopleDiv.innerText = numberOfPeople;
+
+  calculateBill();
+};
